5cfbe70929fb0ae11dd2c2c738c9db12e843068f
Zundel <Andrew.Zundel@us.crowncastle.com>
2017-08-07 12:13:18 -0400

    Add initial table and data structure

---
+++ dayOfWeek/day_of_week_table.sql
@@ -0,0 +1,14 @@
+-- Table creation
+
+CREATE TABLE WOD.DAY_OF_WEEK
+(
+  day_id         int    NOT NULL, -- 1, 2, 3
+  day_code       varchar2(5)  NOT NULL, -- SUN, MON, TUES
+  day_short_name varchar2(5)  NOT NULL, -- Sun, Mon, Tues
+  day_name       varchar2(10) NOT NULL,  -- Sunday, Monday, Tuesday
+  CONSTRAINT day_pk PRIMARY KEY (day_id),
+  CONSTRAINT day_code_check CHECK (day_code IN ('SUN', 'MON', 'TUES', 'WED', 'THURS', 'FRI', 'SAT')),
+  CONSTRAINT day_id_check CHECK (day_id BETWEEN 1 AND 7)
+);
+
+
---
+++ wordsOfDay/words_of_day_data.sql
@@ -0,0 +1,46 @@
+-- Data Initialization
+
+INSERT INTO WOD.words_of_day (day_id, word_name)
+VALUES(1, 'Calm');
+INSERT INTO WOD.words_of_day (day_id, word_name)
+VALUES(1, 'Mindful');
+INSERT INTO WOD.words_of_day (day_id, word_name)
+VALUES(1, 'Rested');
+
+INSERT INTO WOD.words_of_day (day_id, word_name)
+VALUES(2, 'Respectful');
+INSERT INTO WOD.words_of_day (day_id, word_name)
+VALUES(2, 'Tenacious');
+INSERT INTO WOD.words_of_day (day_id, word_name)
+VALUES(2, 'Zealous');
+
+INSERT INTO WOD.words_of_day (day_id, word_name)
+VALUES(3, 'Vigilant');
+INSERT INTO WOD.words_of_day (day_id, word_name)
+VALUES(3, 'Perserverant');
+INSERT INTO WOD.words_of_day (day_id, word_name)
+VALUES(3, 'Strength');
+INSERT INTO WOD.words_of_day (day_id, word_name)
+VALUES(3, 'Prowess');
+
+INSERT INTO WOD.words_of_day (day_id, word_name)
+VALUES(4, 'Care');
+INSERT INTO WOD.words_of_day (day_id, word_name)
+VALUES(4, 'Sure-Footed');
+INSERT INTO WOD.words_of_day (day_id, word_name)
+VALUES(4, 'Level-Minded');
+
+INSERT INTO WOD.words_of_day (day_id, word_name)
+VALUES(5, 'Care-Free');
+INSERT INTO WOD.words_of_day (day_id, word_name)
+VALUES(5, 'Open-Minded');
+
+INSERT INTO WOD.words_of_day (day_id, word_name)
+VALUES(6, 'Brazilians');
+
+INSERT INTO WOD.words_of_day (day_id, word_name)
+VALUES(7, 'Adventure');
+INSERT INTO WOD.words_of_day (day_id, word_name)
+VALUES(7, 'Creativity');
+INSERT INTO WOD.words_of_day (day_id, word_name)
+VALUES(1, 'Thoughtful');
